f944246da98c694ad6c0c0293f054ac53ac24fac
pbgaming <44928010+pbgaming@users.noreply.github.com>
2021-03-26 01:16:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,93 +1,32 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const { Client, Util } = require("discord.js");
+const YouTube = require("simple-youtube-api");
+const ytdl = require("ytdl-core");
+const dotenv = require("dotenv").config();
+require("./server.js");
+ 
+const TOKEN = 'ODI0NzY1OTMwNDQ1ODY0OTkw.YF0JIQ.jFiCMOmLWDC-sIVRSvVXmzPiVqY';
+const PREFIX = A
 
-const Util = require('discord.js');
+const bot = new Client({
+    disableEveryone: true
+});
 
-const token = 'ODI0NzY1OTMwNDQ1ODY0OTkw.YF0JIQ.jFiCMOmLWDC-sIVRSvVXmzPiVqY';
+Client.on('message', message => {
+  if (message.content === "Ahelp") {
+message.channel.sendMessage(""+ `  **
 
-const getYoutubeID = require('get-youtube-id');
+شكراً لاٍستعمالك البوت
 
-const fetchVideoInfo = require('youtube-info');
+Aplay ~اسم الاغنية~
 
-const YouTube = require('simple-youtube-api');
+Askip ~لعمل تخطى للاغنية~
 
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-const client = new Discord.Client({disableEveryone: true});
-
-const prefix = "A";
-/////////////////////////
-////////////////////////
-
-client.on('message', async msg =>{
-	if (msg.author.bot) return undefined;
-    if (!msg.content.startsWith(prefix)) return undefined;
-    
-    let args = msg.content.split(' ');
-
-	let command = msg.content.toLowerCase().split(" ")[0];
-	command = command.slice(prefix.length)
-
-    if(command === `ping`) {
-    let embed = new Discord.RichEmbed()
-    .setColor(3447003)
-    .setTitle("Pong!!")
-    .setDescription(`${client.ping} ms,`)
-    .setFooter(`Requested by | ${msg.author.tag}`);
-    msg.delete().catch(O_o=>{})
-    msg.channel.send(embed);
+Astop ~لاقاف الاغنية~
+**`);
     }
 });
-/////////////////////////
-////////////////////////
-//////////////////////
-client.on('message', async msg =>{
-	if (msg.author.bot) return undefined;
-    if (!msg.content.startsWith(prefix)) return undefined;
-    
-    let args = msg.content.split(' ');
 
-	let command = msg.content.toLowerCase().split("$")[0];
-	command = command.slice(prefix.length)
-
-    if(command === `avatar`){
-	if(msg.channel.type === 'dm') return msg.channel.send("Nope Nope!! u can't use avatar command in DMs (:")
-        let mentions = msg.mentions.members.first()
-        if(!mentions) {
-          let sicon = msg.author.avatarURL
-          let embed = new Discord.RichEmbed()
-          .setImage(msg.author.avatarURL)
-          .setColor("#5074b3")
-          msg.channel.send({embed})
-        } else {
-          let sicon = mentions.user.avatarURL
-          let embed = new Discord.RichEmbed()
-          .setColor("#5074b3")
-          .setImage(sicon)
-          msg.channel.send({embed})
-        }
-    };
-});
-/////////////////////////
-////////////////////////
-//////////////////////
-/////////////////////////
-////////////////////////
-//////////////////////
-
-/////////////////////////
-////////////////////////
-//////////////////////
-/////////////////////////
-////////////////////////
-//////////////////////
 client.on('message', async msg => { 
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
